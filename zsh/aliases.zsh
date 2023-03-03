88254b78c26778680f6d8b65dd7c2d19bfc2aa8a
GabrieleBocchi <gabrielebocchi02@gmail.com>
2023-03-03 09:12:03 +0100

    added some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
+# misc
 alias l='ls -lAh'
+alias cat='bat'
+
+# network
 alias vpnup='nmcli connection up Appartamento'
 alias vpndown='nmcli connection down Appartamento'
+
+# ipinfo.io
+alias ipinfo='curl ipinfo.io'
