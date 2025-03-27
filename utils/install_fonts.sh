d7e911e313f2b526748d42f7ff6532269885e4fb
Gabriele Bocchi <40704221+GabrieleBocchi@users.noreply.github.com>
2025-03-27 15:15:54 +0100

    chore: clean install_fonts.sh script

--- utils/install_fonts.sh
+++ utils/install_fonts.sh
@@ -5,13 +5,13 @@ set -e
 FONT="JetBrainsMono"
 FONT_PATH="$FONT.tar.xz"
 
-if ! $(fc-match -a | grep -q $FONT); then
-	curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONT_PATH
-	tar -xf ./$FONT_PATH -C ~/.local/share/fonts/fonts/
-	rm ./$FONT_PATH
-	rm -f ~/.local/share/fonts/fonts/OFL.txt
-	rm -f ~/.local/share/fonts/fonts/README.md
-	fc-cache -f -v
+if ! fc-match -a | grep -q $FONT; then
+    curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONT_PATH
+    tar -xf ./$FONT_PATH -C ~/.local/share/fonts/fonts/
+    rm ./$FONT_PATH
+    rm -f ~/.local/share/fonts/fonts/OFL.txt
+    rm -f ~/.local/share/fonts/fonts/README.md
+    fc-cache -f -v
 else
-	echo "$FONT is already installed"
+    echo "$FONT is already installed"
 fi
